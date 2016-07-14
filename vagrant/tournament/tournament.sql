e86004b81b77525718509fc11f4274ac75b6caa3
mataelle <mataelle@gmail.com>
2016-07-14 15:58:04 +0300

    added first version of tables

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,24 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE player (
+    id integer PRIMARY KEY,
+    username varchar(40) NOT NULL
+);
 
+CREATE TABLE tournament (
+    id integer PRIMARY KEY
+);
+
+CREATE TABLE round (
+    id integer PRIMARY KEY,
+    number integer NOT NULL,
+    tournament integer references tournament(id)
+);
+
+CREATE TABLE match (
+    id integer PRIMARY KEY,
+    round integer references round(id),
+    winner integer references player(id),
+    loser integer references player(id)
+);
