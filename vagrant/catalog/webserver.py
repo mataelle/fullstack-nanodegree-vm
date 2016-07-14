796830d56fb38dd8e4472425705aed1b9e689da2
mataelle <mataelle@gmail.com>
2016-07-15 02:36:35 +0300

    templates added

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Category, Item, User
@@ -11,28 +11,28 @@ app = Flask(__name__)
 
 @app.route('/category/<int:category_id>')
 def CategoryHandler(category_id):
-    return "Here will be category with list of items"
+    return render_template('category.html', category=None, items=None)
 
 @app.route('/category/<int:category_id>/item/<int:item_id>')
 def ItemHandler(category_id, item_id):
-    return "item"
+    return render_template('item.html', category=None, item=None)
 
 @app.route('/category/<int:category_id>/item/new')
 def NewItemHandler(category_id):
-    return "new item"
+    return render_template('item_new.html', category=None, item=None)
 
 @app.route('/category/<int:category_id>/item/<int:item_id>/edit')
 def EditItemHandler(category_id, item_id):
-    return "edit item"
+    return render_template('item_edit.html', category=None, item=None)
 
 @app.route('/category/<int:category_id>/item/<int:item_id>/delete')
 def DeleteItemHandler(category_id, item_id):
-    return "delete item"
+    return render_template('item_delete.html', category=None, item=None)
 
 @app.route('/category')
 @app.route('/')
 def MainHandler():
-    return "Main"
+    return render_template('category.html', category=None, latest_items=None)
 
 if __name__ == "__main__":
     app.debug = True
