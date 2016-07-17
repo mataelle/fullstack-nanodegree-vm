c7a11215127bd7fff6150d373e5a00a2179d276e
mataelle <mataelle@gmail.com>
2016-07-17 10:53:49 +0300

    added accordeons, works without user

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -1,8 +1,9 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -23,6 +24,7 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    items = relationship('Item', back_populates="category")
 
     @property
     def serialize(self):
@@ -39,9 +41,10 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
-    description = Column(String(250))
+    description = Column(String(1500))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    created = Column(DateTime(timezone=True), default=func.now())
     # user_id = Column(Integer, ForeignKey('user.id'))
     # user = relationship(User)
 
@@ -54,7 +57,6 @@ class Item(Base):
             'description': self.description,
             'cat_id': self.category_id,
         }
-
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.create_all(engine)
--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -11,28 +11,83 @@ app = Flask(__name__)
 
 @app.route('/category/<int:category_id>')
 def CategoryHandler(category_id):
-    return render_template('category.html', category=None, items=None)
+    category = session.query(Category).filter_by(id = category_id).one()
+    return render_template('category.html', category=category)
 
 @app.route('/category/<int:category_id>/item/<int:item_id>')
 def ItemHandler(category_id, item_id):
-    return render_template('item.html', category=None, item=None)
+    item = session.query(Item).filter_by(id = item_id).one()
+    return render_template('item.html', item=item)
 
-@app.route('/category/<int:category_id>/item/new')
+@app.route('/category/<int:category_id>/item/new', methods = ['GET', 'POST'])
 def NewItemHandler(category_id):
-    return render_template('item_new.html', category=None, item=None)
+    if request.method == 'GET':
+        category = session.query(Category).filter_by(id = category_id).one()
+        return render_template('item_new.html',
+                               category = category)
 
-@app.route('/category/<int:category_id>/item/<int:item_id>/edit')
+    else:
+        name = request.form.get('item_name', None)
+        description = request.form.get('description', None)
+        category = session.query(Category).filter_by(id = category_id).one()
+        if not name or not description:
+           return render_template('item_new.html',
+                                  category = category,
+                                  name = name,
+                                  description = description,
+                                  err_msg = True)
+        item = Item(name = name, description = description, category=category)
+        session.add(item)
+        session.commit()
+        return redirect(url_for('ItemHandler', category_id = category_id, item_id = item.id))
+
+@app.route('/category/<int:category_id>/item/<int:item_id>/edit', methods = ['GET', 'POST'])
 def EditItemHandler(category_id, item_id):
-    return render_template('item_edit.html', category=None, item=None)
+    if request.method == 'GET':
+        category = session.query(Category).filter_by(id = category_id).one()
+        item = session.query(Item).filter_by(id = item_id).one()
+        return render_template('item_edit.html',
+                               item_id = item_id,
+                               category = category,
+                               name = item.name,
+                               description = item.description)
+    else:
+        name = request.form.get('item_name', None)
+        description = request.form.get('description', None)
+        if not name or not description:
+           return render_template('item_edit.html',
+                                  item_id = item_id,
+                                  name = name,
+                                  description = description,
+                                  err_msg = True)
+        category = session.query(Category).filter_by(id = category_id).one()
+        item = session.query(Item).filter_by(id = item_id).one()
+        item.name = name
+        item.description = description
+        session.add(item)
+        session.commit()
+        return render_template('item.html', category=category, item=item)
 
-@app.route('/category/<int:category_id>/item/<int:item_id>/delete')
+@app.route('/category/<int:category_id>/item/<int:item_id>/delete', methods = ['GET', 'POST'])
 def DeleteItemHandler(category_id, item_id):
-    return render_template('item_delete.html', category=None, item=None)
+    if request.method == 'GET':
+        category = session.query(Category).filter_by(id = category_id).one()
+        return render_template('item_delete.html',
+                                category=category,
+                                item_id = item_id)
+    else:
+        category = session.query(Category).filter_by(id = category_id).one()
+        item = session.query(Item).filter_by(id = item_id).one()
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('CategoryHandler', category_id = category_id))
 
 @app.route('/category')
 @app.route('/')
 def MainHandler():
-    return render_template('front.html', category=None, latest_items=None)
+    categories = session.query(Category).all()
+    latest_items = session.query(Item).order_by('created').limit(10)
+    return render_template('front.html', categories=categories, latest_items=latest_items)
 
 if __name__ == "__main__":
     app.debug = True
